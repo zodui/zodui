b0ede565540d3477739064c65f2a3ff6d0c44d9a
yijie4188 <yijie4188@gmail.com>
2023-03-23 14:37:14 +0800

    feat(react): error handler

---
+++ packages/react/src/error-handler.tsx
@@ -0,0 +1,51 @@
+import {
+  createContext, Dispatch,
+  PropsWithChildren, ReactElement, SetStateAction,
+  useCallback,
+  useContext, useEffect,
+  useMemo,
+  useState
+} from 'react'
+
+interface ErrorHandler {
+  error: Error
+  throwError: (error: string | Error) => ReactElement
+  ThrowError: (props: {
+    error: string | Error
+  }) => ReactElement
+}
+
+const ErrorHandler = createContext<ErrorHandler>(null)
+
+function ErrorComp({ error, setE }: { error: Error, setE: Dispatch<SetStateAction<Error>> }) {
+  useEffect(() => {
+    setE(error)
+  }, [error, setE])
+  return <></>
+}
+
+export function useErrorHandler() {
+  const [e, setE] = useState<Error>()
+
+  const errorHandler = useMemo<ErrorHandler>(() => ({
+    error: e,
+    throwError(error) {
+      if (typeof error === 'string') {
+        error = new Error(error)
+      }
+      return <ErrorComp error={error} setE={setE}/>
+    },
+    ThrowError({ error }) {
+      return this.throwError(error)
+    },
+  }), [])
+
+  return {
+    error: e,
+    ErrorHandler: useCallback(({ children }: PropsWithChildren) => <ErrorHandler.Provider value={errorHandler} >
+      {children}
+    </ErrorHandler.Provider>, [errorHandler])
+  }
+}
+
+export const useErrorHandlerContext = () => useContext(ErrorHandler)
