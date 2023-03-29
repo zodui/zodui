492b16bf50bffc645cc64c772db66a9cf3887c41
yijie4188 <yijie4188@gmail.com>
2023-03-29 15:18:31 +0800

    refactor(demo): let monaco as project file to import

--- demo/src/env.d.ts
+++ demo/src/env.d.ts
@@ -1,11 +1,38 @@
 /// <reference types="vite/client" />
+/// <reference path="../public/monaco/monaco" />
 
 export declare global {
   import z from 'zod'
   const z: z
   function onCodeChange(fn: (code: string) => void): () => void
+  const ZOD_DTS_FILES: {
+    content: string;
+    filePath: string;
+  }[]
+  /**
+   * copy content to clipboard
+   */
+  function copyToClipboard(content: string): void
+  /**
+   * base64 encode and decode
+   */
+  function base64(str: string, isDecode = true): string
+  /**
+   * throttle
+   */
+  function throttle<T extends Function>(fn: T, delay: number): T
+  /**
+   * debounce
+   */
+  function debounce<T extends Function>(fn: T, delay: number): T
+  /**
+   * display a message
+   */
+  function showMessage(content: string, duration = 3000): void
   interface Window {
     z: z
+    base64: typeof base64
     onCodeChange: typeof onCodeChange
+    ZOD_DTS_FILES: typeof ZOD_DTS_FILES
   }
 }
---
+++ demo/src/monaco.ts
@@ -0,0 +1,139 @@
+let editor: monaco.editor.IStandaloneCodeEditor
+const changeListeners: Function[] = []
+window.onCodeChange = function (fn) {
+  // TODO refactor as import maps
+  const nfn = (s: string) => fn(`${s}
+;(window?.____DEFAULT_EXPORT_VALUE____)`
+    .replace('export default ', 'window.____DEFAULT_EXPORT_VALUE____ = ')
+    .replace(/import ([\s\S]*) from 'zod'/g, 'var $1 = window.z')
+    .replace(/\* as/g, '')
+    .replace(/import ([\s\S]*) from .*/g, ''))
+  changeListeners.push(nfn)
+  nfn(editor?.getValue())
+  const index = changeListeners.length - 1
+  return () => {
+    if (index > -1) changeListeners.splice(index, 1)
+  }
+}
+function updateCode(s: string) {
+  changeListeners.forEach((fn) => fn(s))
+}
+window.addEventListener('load', function () {
+  function setCodeByUrl() {
+    const hash = location.hash.slice(1)
+    const code = hash ? base64(hash) : `
+import * as z from 'zod'
+
+export default z.object({
+  foo: z.string()
+})
+`.trim()
+    editor.setValue(code)
+    updateCode(code)
+  }
+
+  // watch hash change
+  window.addEventListener('hashchange', setCodeByUrl)
+  // add custom dts
+  monaco.languages.typescript.typescriptDefaults.setExtraLibs(ZOD_DTS_FILES)
+  monaco.languages.typescript.typescriptDefaults.addExtraLib(`import z, { Schema, ZodDefaultDef, ZodFirstPartyTypeKind, ZodObject, ZodType, ZodTypeAny } from 'zod'
+
+export interface ModesMap {
+  [ZodFirstPartyTypeKind.ZodNumber]:
+    | 'slider'
+    | 'rate'
+  [ZodFirstPartyTypeKind.ZodString]:
+    | 'textarea'
+    | 'link'
+    | 'secrets'
+    | 'date'
+    | 'datetime'
+    | 'time'
+    | 'panel'
+  [ZodFirstPartyTypeKind.ZodBoolean]:
+    | 'checkbox'
+  [ZodFirstPartyTypeKind.ZodDate]:
+    | 'datetime'
+    | 'time'
+    | 'panel'
+  [ZodFirstPartyTypeKind.ZodTuple]:
+    | 'range'
+    | 'slider'
+    | 'no-range'
+    | 'no-slider'
+    | 'datetime'
+    | 'time'
+    | 'panel'
+}
+
+declare module 'zod' {
+  export interface ZodTypeDef {
+    mode: string
+    label: string
+  }
+  export interface ZodType<Output = any, Def extends ZodTypeDef = ZodTypeDef, Input = Output> {
+    readonly _mode: string
+    mode<T extends string>(
+      mode:
+        | Def extends ZodDefaultDef<infer InnerT>
+          ? ModesMap[InnerT['_def']['typeName']]
+          : ModesMap[Def['typeName']]
+        | (string & {})
+    ): ZodType<Output, Def, Input>
+
+    readonly _label: string
+    label(mode: string): ZodType<Output, Def, Input>
+
+    readonly type: string
+  }
+  export function clazz<T>(clazz: { new(): T }): Schema<T>
+  export function asObejct<T extends any>(t: T): ZodObject<Record<string, ZodTypeAny> & T>
+}`, 'file:///env.d.ts')
+  editor = monaco.editor.create(document.getElementById('container')!, {
+    // theme: 'vs-dark',
+    value: '',
+    language: 'typescript',
+    tabIndex: 2,
+    model: monaco.editor.createModel('', 'typescript', monaco.Uri.parse('file:///main.ts')),
+  })
+
+  setCodeByUrl()
+  const historyCodes: { code: string }[] = []
+  let historyIndex = -1
+  // watch editor value change
+  editor.onDidChangeModelContent(debounce(function () {
+    updateCode(editor.getValue())
+  }, 1500))
+  editor.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyS, () => {
+    const code = editor.getValue()
+    updateCode(code)
+    // save code to hash
+    location.hash = `#${base64(code, false)}`
+    // copy url to clipboard
+    copyToClipboard(location.href)
+    showMessage('<h3 style="margin: 0">url copied to clipboard, share it with your friends!</h3>')
+  })
+  editor.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.UpArrow, function () {
+    if (historyIndex === -1) {
+      historyIndex = historyCodes.length - 1
+    } else {
+      historyIndex--
+    }
+    if (historyIndex < 0) {
+      historyIndex = 0
+    }
+    editor.setValue(historyCodes[historyIndex].code)
+  })
+  editor.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.DownArrow, function () {
+    if (historyIndex === -1) {
+      historyIndex = historyCodes.length - 1
+    } else {
+      historyIndex++
+    }
+    if (historyIndex >= historyCodes.length) {
+      historyIndex = historyCodes.length - 1
+    }
+    editor.setValue(historyCodes[historyIndex].code)
+  })
+  editor.focus()
+})
