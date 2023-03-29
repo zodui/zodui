ba64579c1ae5e73111450f4796ac510908a52448
Olnyhhhh <55044991+Only566@users.noreply.github.com>
2023-03-29 23:42:41 +0800

    feat(react): support display label and desc for obj (#6)

--- packages/react/src/schema.tsx
+++ packages/react/src/schema.tsx
@@ -1,3 +1,4 @@
+import './schema.scss'
 import type { Schema as ZodSchema } from 'zod'
 
 import { Item } from './item'
@@ -7,6 +8,8 @@ export interface SchemaProps {
   model: ZodSchema
   disabled?: boolean
 }
+
+const prefix = 'zodui-schema'
 
 export function Schema(props: SchemaProps) {
   const {
@@ -24,6 +27,13 @@ export function Schema(props: SchemaProps) {
 
   // TODO resolve object lbale
   return <>
+    <div className={`${prefix}__header`}>
+      {props.model._def.label && <h3 className={`${prefix}__label`}>
+        {props.model._def.label}
+      </h3>}
+      {props.model._def.description
+        && <div className={`${prefix}__desc`}>{props.model._def.description}</div>}
+    </div>
     {Object.entries(model._def.shape()).map(([key, value]) => <Item
       key={key}
       label={value._def.label || key}
