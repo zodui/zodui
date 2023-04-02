7efcf68145151b9758b6bc2d56f41a118ab8df0f
yijie4188 <yijie4188@gmail.com>
2023-04-02 17:31:30 +0800

    chore(demo): refactor inject options data

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -5,35 +5,38 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { createHtmlPlugin } from 'vite-plugin-html'
-import * as process from 'process'
-
-const nodeModulesPath = path.join(__dirname, '../node_modules')
-const zodPackagePath = path.join(nodeModulesPath, 'zod/lib')
 
 const ZOD_DTS_FILES: { content: string, filePath: string }[] = []
 
-function addZodDtsFileContent(filePath: string) {
-  let content = fs.readFileSync(filePath, 'utf-8')
-  ZOD_DTS_FILES.push({
-    content,
-    filePath: filePath.replace(zodPackagePath, 'file:///node_modules/@types/zod')
-  })
-}
+initZOD_DTS_FILES: {
+  const nodeModulesPath = path.join(__dirname, '../node_modules')
+  const zodPackagePath = path.join(nodeModulesPath, 'zod/lib')
 
-function findZodDtsFiles(dirPath: string) {
-  const files = fs.readdirSync(dirPath)
-  for (const file of files) {
-    const filePath = path.join(dirPath, file)
-    const stats = fs.statSync(filePath)
-    if (stats.isDirectory()) {
-      findZodDtsFiles(filePath)
-    } else if (stats.isFile() && path.basename(filePath).endsWith('.d.ts')) {
-      addZodDtsFileContent(filePath)
+  function addZodDtsFileContent(filePath: string) {
+    let content = fs.readFileSync(filePath, 'utf-8')
+    ZOD_DTS_FILES.push({
+      content,
+      filePath: filePath.replace(zodPackagePath, 'file:///node_modules/@types/zod')
+    })
+  }
+  function findZodDtsFiles(dirPath: string) {
+    const files = fs.readdirSync(dirPath)
+    for (const file of files) {
+      const filePath = path.join(dirPath, file)
+      const stats = fs.statSync(filePath)
+      if (stats.isDirectory()) {
+        findZodDtsFiles(filePath)
+      } else if (stats.isFile() && path.basename(filePath).endsWith('.d.ts')) {
+        addZodDtsFileContent(filePath)
+      }
     }
   }
+  findZodDtsFiles(zodPackagePath)
 }
 
-findZodDtsFiles(zodPackagePath)
+const commonInjectData: Record<string, string> = {
+  HEADER: fs.readFileSync(path.resolve(process.cwd(), './public/header.html')).toString(),
+}
 
 export default defineConfig({
   base: '/zodui/',
@@ -47,6 +50,7 @@ export default defineConfig({
           template: 'index.html',
           injectOptions: {
             data: {
+              ...commonInjectData,
               ZOD_DTS_FILES: JSON.stringify([])
             }
           }
@@ -56,7 +60,7 @@ export default defineConfig({
           template: 'public/play.html',
           injectOptions: {
             data: {
-              HEADER: fs.readFileSync(path.resolve(process.cwd(), './public/header.html')),
+              ...commonInjectData,
               ZOD_DTS_FILES: JSON.stringify(ZOD_DTS_FILES)
             }
           }
