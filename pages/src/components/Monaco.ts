aec64ca4d327193d9d12d6ddb0c3e66a48461b0e
yijie4188 <yijie4188@gmail.com>
2023-04-04 21:26:54 +0800

    feat: update home index as astro

---
+++ pages/src/components/Monaco.ts
@@ -0,0 +1,198 @@
+const DEFAULT_CODE = `import * as z from 'zod'
+
+export default z
+  .object({
+    foo: z.string()
+  })
+  .label('Object')
+  .describe('This is a Object Configuration')
+`
+
+const ZOD_EXTERNAL = `import z, { Schema, ZodDefaultDef, ZodFirstPartyTypeKind, ZodObject, ZodType, ZodTypeAny } from 'zod'
+
+export interface ModesMap {
+  [ZodFirstPartyTypeKind.ZodNumber]:
+    | 'slider'
+    | 'rate'
+  [ZodFirstPartyTypeKind.ZodString]:
+    | 'textarea'
+    | 'link'
+    | 'secrets'
+    | 'date'
+    | 'datetime'
+    | 'time'
+    | 'panel'
+  [ZodFirstPartyTypeKind.ZodBoolean]:
+    | 'checkbox'
+  [ZodFirstPartyTypeKind.ZodDate]:
+    | 'datetime'
+    | 'time'
+    | 'panel'
+  [ZodFirstPartyTypeKind.ZodTuple]:
+    | 'range'
+    | 'slider'
+    | 'no-range'
+    | 'no-slider'
+    | 'datetime'
+    | 'time'
+    | 'panel'
+}
+
+declare module 'zod' {
+  export interface ZodTypeDef {
+    mode: string
+    label: string
+  }
+  export interface ZodType<Output = any, Def extends ZodTypeDef = ZodTypeDef, Input = Output> {
+    readonly _mode: string
+    mode<T extends string>(
+      mode:
+        | Def extends ZodDefaultDef<infer InnerT>
+          ? ModesMap[InnerT['_def']['typeName']]
+          : ModesMap[Def['typeName']]
+        | (string & {})
+    ): ZodType<Output, Def, Input>
+
+    readonly _label: string
+    label(mode: string): ZodType<Output, Def, Input>
+
+    readonly type: string
+  }
+  export function clazz<T>(clazz: { new(): T }): Schema<T>
+  export function asObejct<T extends any>(t: T): ZodObject<Record<string, ZodTypeAny> & T>
+}`
+
+let editor: monaco.editor.IStandaloneCodeEditor
+
+window.onThemeChange(theme => {
+  if (editor) {
+    monaco.editor.setTheme(theme === 'dark' ? 'vs-dark' : 'vs')
+  } else {
+    let i = setInterval(() => {
+      if (editor) {
+        monaco.editor.setTheme(theme === 'dark' ? 'vs-dark' : 'vs')
+        clearInterval(i)
+      }
+    }, 100)
+  }
+})
+
+const changeListeners: Function[] = []
+
+window.onCodeChange = function (fn) {
+  // TODO refactor as import maps
+  const nfn = (s: string) => fn(`${s}
+;(window?.____DEFAULT_EXPORT_VALUE____)`
+    .replace('export default ', 'window.____DEFAULT_EXPORT_VALUE____ = ')
+    .replace(/import ([\s\S]*) from 'zod'/g, 'var $1 = window.z')
+    .replace(/\* as/g, '')
+    .replace(/import ([\s\S]*) from .*/g, ''))
+  changeListeners.push(nfn)
+  nfn(editor?.getValue())
+  const index = changeListeners.length - 1
+  return () => {
+    if (index > -1) changeListeners.splice(index, 1)
+  }
+}
+function updateCode(s: string) {
+  changeListeners.forEach((fn) => fn(s))
+}
+window.addEventListener('load', () => {
+  function setCodeByUrl() {
+    const hash = location.hash.slice(1)
+    const code = hash ? base64(hash) : DEFAULT_CODE
+    editor.setValue(code)
+    updateCode(code)
+  }
+
+  // watch hash change
+  window.addEventListener('hashchange', setCodeByUrl)
+  // add custom dts
+  monaco.languages.typescript.typescriptDefaults.setExtraLibs(
+    ZOD_DTS_FILES
+      .concat([{ content: ZOD_EXTERNAL, filePath: 'file:///env.d.ts' }])
+  )
+  editor = monaco.editor.create(document.getElementById('container')!, {
+    theme: window.theme === 'dark' ? 'vs-dark' : 'vs',
+    value: '',
+    language: 'typescript',
+    tabSize: 2,
+    automaticLayout: true,
+    model: monaco.editor.createModel('', 'typescript', monaco.Uri.parse('file:///main.ts')),
+  })
+
+  setCodeByUrl()
+  const historyCodes: { code: string }[] = []
+  let historyIndex = -1
+  // watch editor value change
+  editor.onDidChangeModelContent(debounce(function () {
+    updateCode(editor.getValue())
+  }, 1500))
+  editor.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyS, () => {
+    const code = editor.getValue()
+    updateCode(code)
+    // save code to hash
+    location.hash = `#${base64(code, false)}`
+    // copy url to clipboard
+    copyToClipboard(location.href)
+    showMessage('<h3 style="margin: 0">url copied to clipboard, share it with your friends!</h3>')
+  })
+  editor.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.UpArrow, function () {
+    if (historyIndex === -1) {
+      historyIndex = historyCodes.length - 1
+    } else {
+      historyIndex--
+    }
+    if (historyIndex < 0) {
+      historyIndex = 0
+    }
+    editor.setValue(historyCodes[historyIndex].code)
+  })
+  editor.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.DownArrow, function () {
+    if (historyIndex === -1) {
+      historyIndex = historyCodes.length - 1
+    } else {
+      historyIndex++
+    }
+    if (historyIndex >= historyCodes.length) {
+      historyIndex = historyCodes.length - 1
+    }
+    editor.setValue(historyCodes[historyIndex].code)
+  })
+  editor.focus()
+})
+
+window.addEventListener('load', () => {
+  const root = document.getElementById('root')!
+  const BORDER_SIZE = 4
+
+  let mPos: number
+  function resize(e: MouseEvent) {
+    const dx = mPos - e.x
+    const newWidth = (parseInt(getComputedStyle(root, '').width) + dx)
+    if (newWidth < 550)
+      return
+
+    mPos = e.x
+    root.style.width = newWidth + 'px'
+  }
+
+  let isClick = false
+  root.addEventListener('mousedown', e => {
+    if (e.offsetX < BORDER_SIZE) {
+      mPos = e.x
+      if (!isClick) {
+        isClick = true
+        setTimeout(() => isClick = false, 1000)
+      } else {
+        root.style.width = '700px'
+      }
+      document.addEventListener('mousemove', resize, false)
+      root.style.userSelect = 'none'
+    }
+  }, false)
+  document.addEventListener('mouseup', () => {
+    root.style.userSelect = 'auto'
+    document.removeEventListener('mousemove', resize, false)
+  })
+})
--- pages/src/env.d.ts
+++ pages/src/env.d.ts
@@ -1,8 +1,63 @@
 /// <reference types="astro/client" />
+/// <reference path="../public/monaco/monaco" />
 
 interface ImportMetaEnv {
 }
 
 interface ImportMeta {
   readonly env: ImportMetaEnv;
+}
+
+export declare global {
+  import z from 'zod'
+  const z: z
+  const ZOD_DTS_FILES: {
+    content: string;
+    filePath: string;
+  }[]
+  /**
+   * copy content to clipboard
+   */
+  function copyToClipboard(content: string): void
+  /**
+   * base64 encode and decode
+   */
+  function base64(str: string, isDecode = true): string
+  /**
+   * throttle
+   */
+  function throttle<T extends Function>(fn: T, delay: number): T
+  /**
+   * debounce
+   */
+  function debounce<T extends Function>(fn: T, delay: number): T
+  /**
+   * display a message
+   */
+  function showMessage(content: string, duration = 3000): void
+
+  const theme: 'light' | 'dark' | (string & {})
+  function onThemeChange(fn: (t: typeof theme) => void)
+
+  function onCodeChange(fn: (code: string) => void): () => void
+
+  interface Tab {
+    href: string
+    title: string
+    disabled?: boolean
+  }
+  const TABS: Tab[]
+
+  interface Window {
+    z: z
+
+    theme: typeof theme
+    base64: typeof base64
+
+    onThemeChange: typeof onThemeChange
+    onCodeChange: typeof onCodeChange
+
+    TABS: typeof TABS
+    ZOD_DTS_FILES: typeof ZOD_DTS_FILES
+  }
 }
