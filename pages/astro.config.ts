a484d2af49f65caf90d1a6078d80bc2fb612b11d
yijie4188 <yijie4188@gmail.com>
2023-04-05 12:00:38 +0800

    chore(pages): import astro pages

--- pages/astro.config.ts
+++ pages/astro.config.ts
@@ -1,6 +1,28 @@
 import { defineConfig } from 'astro/config'
 
+import react from '@astrojs/react';
+// import preact from '@astrojs/preact';
+// import svelte from '@astrojs/svelte';
+import vue from '@astrojs/vue';
+// import solid from '@astrojs/solid-js';
+// import lit from '@astrojs/lit';
+// import alpine from '@astrojs/alpinejs';
+
 // https://astro.build/config
 export default defineConfig({
-  base: '/zodui/'
-})
+  base: '/zodui/',
+  integrations: [
+    vue(),
+    // svelte(),
+    react(),
+    // preact(),
+    // solid(),
+    // lit(),
+    // alpine()
+  ],
+  vite: {
+    ssr: {
+      noExternal: [/tdesign-react/]
+    }
+  }
+});
