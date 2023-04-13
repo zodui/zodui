d0287256999f861977fb715796a5a56f096c0ca9
yijie4188 <yijie4188@gmail.com>
2023-04-13 12:01:07 +0800

    feat(react): schema value logic

--- packages/react/src/schema.tsx
+++ packages/react/src/schema.tsx
@@ -2,15 +2,18 @@ import './schema.scss'
 import type { Schema as ZodSchema } from 'zod'
 
 import { Item } from './item'
-import { AllTypes, inlineMarkdown, isWhatType } from './utils'
+import { AllTypes, inlineMarkdown, isWhatType, merge } from './utils'
 import { plgMaster } from './plugins'
 import common from './plugins/common'
-import { useEffect } from 'react'
+import { useCallback, useEffect, useRef } from 'react'
 
 export interface SchemaProps {
   prefix?: string
   model: ZodSchema
   disabled?: boolean
+  value?: any
+  defaultValue?: any
+  onChange?: (value: any) => (void | Promise<void>)
 }
 
 const prefix = 'zodui-schema'
@@ -22,28 +25,32 @@ export function Schema(props: SchemaProps) {
 
   const {
     model,
+    value,
+    defaultValue,
+    onChange,
     disabled
   } = props
+  const valueRef = useRef(value ?? defaultValue ?? {})
+  const changeValue = useCallback((v: any) => {
+    valueRef.current = v
+    onChange?.(v)
+  }, [onChange])
 
   if (isWhatType(model, AllTypes.ZodIntersection)) {
     return <>
       <Schema prefix='intersect::left'
               disabled={disabled}
               model={model._def.left}
+              value={valueRef.current}
+              onChange={async v => changeValue(merge(valueRef.current, v))}
       />
       <Schema prefix='intersect::right'
               disabled={disabled}
               model={model._def.right}
+              value={valueRef.current}
+              onChange={async v => changeValue(merge(valueRef.current, v))}
       />
     </>
-  }
-  if (!isWhatType(model, AllTypes.ZodObject)) {
-    return <Item
-      uniqueKey='single'
-      label={model._label || model._def?.description || model.type}
-      schema={model}
-      disabled={disabled}
-    />
   }
 
   return <div className={prefix}>
@@ -57,13 +64,23 @@ export function Schema(props: SchemaProps) {
           dangerouslySetInnerHTML={{ __html: inlineMarkdown(props.model._def.description) }}
         />}
     </div>
-    {Object.entries(model._def.shape()).map(([key, value]) => <Item
-      key={key}
-      uniqueKey={`${props.prefix || ''}.${key}`}
-      label={value._def.label || key}
-      schema={value}
-      disabled={disabled}
-    />)}
+    {isWhatType(model, AllTypes.ZodObject)
+      ? Object.entries(model._def.shape()).map(([key, value]) => <Item
+        key={key}
+        uniqueKey={`${props.prefix || ''}.${key}`}
+        label={value._def.label || key}
+        schema={value}
+        disabled={disabled}
+        onChange={async v => changeValue({ ...valueRef.current, [key]: v })}
+      />)
+      : <Item
+        uniqueKey='single'
+        label={model._label || model._def?.description || model.type}
+        schema={model}
+        disabled={disabled}
+        value={valueRef.current}
+        onChange={changeValue}
+      />}
   </div>
 }
 
