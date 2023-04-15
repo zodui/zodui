9cb2a17d17829317b95f202c3098702bd6494539
yijie4188 <yijie4188@gmail.com>
2023-04-15 16:49:10 +0800

    feat(demo): preview draw-bar operate

--- demo/src/builder/marked-plugins/preview.client.ts
+++ demo/src/builder/marked-plugins/preview.client.ts
@@ -8,14 +8,24 @@ document.querySelectorAll<HTMLDivElement>('.zodui-preview')
     } = ele.dataset
     const originalCode = decodeURIComponent(code)
     emitCode(schemaEvalKey, originalCode)
-    const [exchange, mdBody, evaler] = ele.children
-    let isEvalerVisible = false
-    exchange.addEventListener('click', function () {
+    const [exchange, mdBody, evaler] = ele.querySelector('.wrap')!.children
+    let isEvalerVisible = true
+    exchange.addEventListener('click', () => {
       if (isEvalerVisible) {
         evaler.classList.add('hidden')
       } else {
         evaler.classList.remove('hidden')
       }
       isEvalerVisible = !isEvalerVisible
+    })
+    const drawBar = ele.querySelector('.draw-bar')!
+    let isDrawBarVisible = false
+    drawBar.querySelector('.icon[data-key="open/close"]')!.addEventListener('click', () => {
+      if (isDrawBarVisible) {
+        drawBar.classList.remove('display')
+      } else {
+        drawBar.classList.add('display')
+      }
+      isDrawBarVisible = !isDrawBarVisible
     })
   })
