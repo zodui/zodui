ae2fdd4bbacc88d1ada8b910dfa2647973af7f08
Only566 <sunshine4188y@gmail.com>
2023-04-16 18:20:59 +0800

    feat(react): auto scroll by hash

--- demo/src/builder/marked-plugins/header.ts
+++ demo/src/builder/marked-plugins/header.ts
@@ -7,6 +7,7 @@ export default defineMDPlugin({
     const tagName = `h${level}`
     const sluggerText = slugger.slug(raw);
 
+    // TODO turn href to click function, inorder to smooth scroll
     return `<${tagName} class='anchor-point' id='${sluggerText}'>
       <a href='#${sluggerText}'>#</a>
       ${raw}
