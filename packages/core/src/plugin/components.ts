69974ee7c08c91759cec8b5f6838f12593ca60a8
yijie4188 <yijie4188@gmail.com>
2023-04-23 19:01:58 +0800

    refactor(core): remove useless code

--- packages/core/src/plugin/components.ts
+++ packages/core/src/plugin/components.ts
@@ -1,22 +1,5 @@
 import { FrameworksKeys } from './framework'
 
-export type Icons =
-  | 'Add'
-  | 'More'
-  | 'Link'
-  | 'Clear'
-  | 'Delete'
-  | 'Append'
-  | 'Prepend'
-  | 'ArrowUp'
-  | 'ArrowDown'
-
-export type IconComponentProps<T extends Record<string, any>> =
-  & T
-  & {
-    size?: 'small' | 'medium' | 'large' | string | number;
-  }
-
 export interface FrameworkComponents {
   [key: string]: {
     [K in FrameworksKeys]: any
