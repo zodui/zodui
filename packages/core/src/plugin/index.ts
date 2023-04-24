ba5ba518fb0b2864196621f74e7a8662e3160e26
yijie4188 <yijie4188@gmail.com>
2023-04-24 22:14:03 +0800

    feat(core): context emitter system

--- packages/core/src/plugin/index.ts
+++ packages/core/src/plugin/index.ts
@@ -9,7 +9,44 @@ export type {
 export class Context<
   PluginName extends string = string
 > {
+  constructor(
+    private readonly store?: Map<string, any>
+  ) {
+    if (!store) {
+      this.store = new Map()
+    }
+  }
+  extend() {
+    return new Context(this.store)
+  }
+  emitter = {
+    listeners: new Map<string, Function[]>(),
+    on: (key: string, func: Function) => {
+      const list = this.emitter.listeners.get(key) || []
+      list.push(func)
+      this.emitter.listeners.set(key, list)
+    },
+    do: (...args: any[]) => {
+      const [key, ...params] = args
+      const list = this.emitter.listeners.get(key) || []
+      list.forEach(func => func(...params))
+    },
+  }
+  set(k: string, v?: any) {
+    this.store!.set(k, v)
+    this.emitter.do(k, v)
+    return this
+  }
+  get<T>(k: string) {
+    return [
+      this.store!.get(k) as T,
+      this.emitter.on.bind(this, k),
+    ] as const
+  }
   use(p: Plugin | (() => Promise<Plugin>) | (() => Promise<{ default: Plugin }>)) {
+    if (typeof p === 'function') {
+    } else {
+    }
     return this
   }
   framework<K extends FrameworksKeys>(key: K): Framework<K, PluginName> {
