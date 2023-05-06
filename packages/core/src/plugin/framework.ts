2985a648866d14cc078444ee09c06229e07eac5b
yijie4188 <yijie4188@gmail.com>
2023-05-06 10:27:55 +0800

    feat(core): support define controller

--- packages/core/src/plugin/framework.ts
+++ packages/core/src/plugin/framework.ts
@@ -1,9 +1,96 @@
 import type { Context, Icons } from '@zodui/core'
+import type { TypeMap } from '@zodui/core'
+import type { ControllerProps } from '@zodui/react'
+import type { ReactElement } from 'react'
+
+const PropsSymbol = Symbol('props')
+
+export type AsProps<T = {}> = T & { [PropsSymbol]: unknown }
+
+export interface ControllerPropsMap {
+}
+
+interface BuiltinControllerPropsMap {
+  Array: Record<string, AsProps<ControllerProps<TypeMap['ZodArray']>>>
+  Color: Record<string, AsProps<ControllerProps<TypeMap['ZodString']>>>
+  String: Record<string, AsProps<ControllerProps<TypeMap['ZodString']>>>
+  Number: Record<string, AsProps<ControllerProps<TypeMap['ZodNumber']>>>
+  Boolean: Record<string, AsProps<ControllerProps<TypeMap['ZodBoolean']>>>
+  Date: Record<string, AsProps<ControllerProps<TypeMap['ZodDate']>>>
+}
+
+type CalcPropsMap<Map> = {
+  [K in keyof Map]: K extends keyof BuiltinControllerPropsMap
+    ? {
+      [KK in keyof Map[K]]: BuiltinControllerPropsMap[K][string] & Map[K][KK]
+    }
+    : {}
+}
+
+export type InnerControllerPropsMap<Map = {}> =
+  & BuiltinControllerPropsMap
+  & CalcPropsMap<ControllerPropsMap>
+  & CalcPropsMap<Map>
+
+export type CalcPaths<Map = ControllerPropsMap, PrevK extends string = never> = Map extends AsProps
+  ? PrevK
+  : {
+    [K in (keyof Map & string)]:
+      | [K] extends [never] ? never : PrevK
+      | CalcPaths<Map[K], [PrevK] extends [never] ? K : `${PrevK}${
+        // Equal
+        (<G>() => G extends K ? 1 : 2) extends (<G>() => G extends string ? 1 : 2)
+          ? `.${K}`
+          : `:${K}`
+      }`>
+  }[keyof Map & string]
+
+// export type AllPathsTuple<Map = ControllerPropsMap, PrevK extends string[] = never> = Map extends AsProps
+//   ? PrevK
+//   : {
+//     [K in (keyof Map & string)]:
+//       | [K] extends [never] ? never : PrevK
+//       | AllPathsTuple<Map[K], [PrevK] extends [never] ? [K] : [...PrevK, K]>
+//   }[keyof Map & string]
+//
+// export type JoinString<T extends unknown[], RootCall = false> =
+//   T extends [infer First extends string, ...infer Rest]
+//     ? `${
+//       RootCall extends false
+//         ? (<G>() => G extends First ? 1 : 2) extends (<G>() => G extends string ? 1 : 2)
+//           ? `:${First}` : `.${First}`
+//         : First
+//     }${JoinString<Rest>}`
+//     : ''
+//
+// export type AllPaths<Map = ControllerPropsMap> = AllPathsTuple<Map> extends infer T extends string[]
+//   ? JoinString<T, true>
+//   : never
+
+export type AllPaths<M> = CalcPaths<M> | CalcPaths<BuiltinControllerPropsMap>
+
+export type RevealPropsByPath<
+  Path extends string,
+  Map = InnerControllerPropsMap
+> = Path extends `${infer Key}${'.' | ':'}${infer Rest}`
+  ? Key extends keyof Map
+    ? Map[Key] extends Record<string, any>
+      ? RevealPropsByPath<Rest, Map[Key]>
+      : never
+    : never
+  : Path extends keyof Map
+    ? Map[Path] extends AsProps
+      ? Map[Path]
+      : Map[Path] extends Record<string, any>
+        ? Map[Path][string]
+        : never
+    : never
 
 export class Framework<
   K extends FrameworksKeys,
   N extends string = string,
   Components = Frameworks[K]['Components'],
+  Controllers = Frameworks[K]['Controllers'],
 > {
   constructor(
     private readonly key: K,
@@ -27,7 +114,14 @@ export class Framework<
     this.ctx.set(`framework.${this.key}.icons.${icon}`, Comp)
     return this
   }
-  // defineCtrl(): Framework<K>
+  defineCtrl<
+    P extends AllPaths<ControllerPropsMap & Controllers>,
+    InnerProps = RevealPropsByPath<P, InnerControllerPropsMap<Controllers>>,
+    Props = InnerProps extends AsProps<infer P> ? P : never
+  >(path: P | (string & {}), Ctrl: (props: Props) => ReactElement) {
+    this.ctx.set(`framework.${this.key}.ctrls.${path}`, Ctrl)
+    return this
+  }
   // defineView(): Framework<K>
   // defineStructure(): Framework<K>
 }
@@ -39,6 +133,8 @@ export interface Frameworks {
      * define framework special component func value to infer defineComp func type
      */
     Components: Record<string, any>
+    Controllers: {
+    }
   }
 }
 
