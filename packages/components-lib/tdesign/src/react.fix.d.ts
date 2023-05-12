22acf83070272df285082c7595e515759e054f0d
yijie4188 <yijie4188@gmail.com>
2023-05-12 17:59:30 +0800

    fix(components-lib): tdesign react type for slider, add generic support for it

---
+++ packages/components-lib/tdesign/src/react.fix.d.ts
@@ -0,0 +1,20 @@
+import type { CSSProperties } from 'react'
+import type {} from 'tdesign-react/esm'
+
+interface StyledProps {
+  className?: string
+  style?: CSSProperties
+}
+
+declare module 'tdesign-react/esm' {
+  import type { JSXElementConstructor, ReactElement, Ref } from 'react'
+  import type { SliderValue, TdSliderProps } from 'tdesign-react/esm'
+  interface SliderProps<T extends SliderValue> extends TdSliderProps {
+    value?: T
+    defaultValue?: T
+    onChange?: (value: T) => void | Promise<void>
+  }
+  export declare const Slider: <T extends SliderValue = SliderValue>(props: SliderProps<T> & StyledProps & {
+    ref?: Ref<HTMLDivElement>
+  }) => ReactElement<any, string | JSXElementConstructor<any>>
+}
