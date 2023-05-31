0996ebc4477b14caea0702ec4528a8afc358ac7f
yijie4188 <yijie4188@gmail.com>
2023-05-31 21:20:35 +0800

    feat(plugins/common): number slider for tuple type

--- packages/plugins/common/src/index.ts
+++ packages/plugins/common/src/index.ts
@@ -1,15 +1,14 @@
 import './index.scss'
 
 import { AllTypes, definePlugin } from '@zodui/core'
-// import type { Schema } from 'zod'
-//
-//
-// function isEqual(schemas: Schema[], types: AllTypes[]) {
-//   return schemas.length === types.length && schemas.every(
-//     (s, i) => (s._def as any)
-//       .typeName === types[i]
-//   )
-// }
+import type { Schema } from 'zod'
+
+function isEqual(schemas: Schema[], types: AllTypes[]) {
+  return schemas.length === types.length && schemas.every(
+    (s, i) => (s._def as any)
+      .typeName === types[i]
+  )
+}
 
 export const CommonPlugin = definePlugin('CommonPlugin', ctx => {
   ctx
@@ -32,13 +31,13 @@ export const CommonPlugin = definePlugin('CommonPlugin', ctx => {
         }]
       ]
     ])
-    // .defineUnit('multiple', [AllTypes.ZodTuple], [
-    //   // TODO support number range input
-    //   [(modes, { schemas }) => !modes.includes('no-slider') && isEqual(schemas, [AllTypes.ZodNumber, AllTypes.ZodNumber]), ({
-    //     schemas: _,
-    //     ...props
-    //   }) => ['Number.Slider', { range: true, ...props }]]
-    // ])
+    .defineUnit('multiple', [AllTypes.ZodTuple], [
+      // TODO support number range input
+      [(modes, { schemas }) => !modes.includes('no-slider') && isEqual(schemas, [AllTypes.ZodNumber, AllTypes.ZodNumber]), ({
+        schemas: _,
+        ...props
+      }) => ['Number.Slider', { range: true, ...props }]]
+    ])
 })
 
 export default CommonPlugin
