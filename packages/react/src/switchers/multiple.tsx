7bb2ba22482a980b86f15786f7751b58389b5917
yijie4188 <yijie4188@gmail.com>
2023-06-13 21:28:02 +0800

    style(react): code style

--- packages/react/src/switchers/multiple.tsx
+++ packages/react/src/switchers/multiple.tsx
@@ -202,9 +202,7 @@ export function Multiple({
   const isComplex = useMemo(() => ComplexMultipleTypes.includes(model._def.typeName), [model._def.typeName])
 
   const errorHandler = useErrorHandlerContext()
-  const Unit = useCoreContextUnit('multiple', model._def.typeName, modes, {
-    schemas
-  })
+  const Unit = useCoreContextUnit('multiple', model._def.typeName, modes, { schemas })
 
   if (schemas.length === 0 && model._def.typeName === AllTypes.ZodTuple) {
     return errorHandler.throwError(new Error('Tuple 类型必须包含一个元素'))
