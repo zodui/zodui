c2bbeeac66aeef567aa1caeb80c86da64db03ff9
yijie4188 <yijie4188@gmail.com>
2023-07-25 10:11:26 +0800

    fix(core): inlineMarkdown function forget `>`

--- packages/core/src/utils.ts
+++ packages/core/src/utils.ts
@@ -66,7 +66,7 @@ export function containSome(a: string[], b: string[]) {
 export function inlineMarkdown(md: string) {
   return md
     .replace(/\[([^\]]+)]\(([^)]+)\)/g, '<a href="$2" target="_blank">$1</a>')
-    .replace(/`([^`]+)`/g, '<code>$1</code')
+    .replace(/`([^`]+)`/g, '<code>$1</code>')
     .replace(/\*\*([^*]+)\*\*/g, '<strong>$1</strong>')
     .replace(/\*([^*]+)\*/g, '<em>$1</em>')
     .replace(/~~([^~]+)~~/g, '<del>$1</del>')
