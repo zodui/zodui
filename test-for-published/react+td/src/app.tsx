486a32c2ce25ba2067d44bf3b14fdc47dcadfe79
yijie4188 <yijie4188@gmail.com>
2023-10-29 22:20:33 +0800

    chore: install dep by `yarn`, because `pnpm` can't install over workspace

--- test-for-published/react+td/src/app.tsx
+++ test-for-published/react+td/src/app.tsx
@@ -1,5 +1,15 @@
+/* eslint-disable @typescript-eslint/no-restricted-imports */
+import '@zodui/react/dist/index.css'
+
+import { List } from '@zodui/react'
+import { z } from 'zod'
+
 export default function App() {
   return <>
     Hello ZodUI!
+    123
+    <List model={z.object({
+      foo: z.string()
+    })} />
   </>
 }
