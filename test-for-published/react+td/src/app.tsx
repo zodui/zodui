1c8a961ea147704cc7067282b3ca15767a2190e8
yijie4188 <yijie4188@gmail.com>
2023-11-05 00:15:09 +0800

    refactor(test): change plugin register method

--- test-for-published/react+td/src/app.tsx
+++ test-for-published/react+td/src/app.tsx
@@ -2,39 +2,31 @@
 import '@zodui/react/dist/index.css'
 import '@zodui/components-lib-tdesign/dist/index.css'
 
+import { TDesignComponentsLibForReact } from '@zodui/components-lib-tdesign/react'
 import { Context } from '@zodui/core'
+import { z } from '@zodui/core/external'
+import type { ListRef } from '@zodui/react'
 import { List } from '@zodui/react'
-import { useEffect, useState } from 'react'
-import { z } from 'zod'
+import { useEffect, useRef } from 'react'
 
 export default function App() {
-  const [ctxInited, setCtxInited] = useState(false)
   useEffect(() => {
-    // let flag = false
-    const e0 = Context.global.use(() => import('@zodui/components-lib-tdesign/react')
-      .then(({ TDesignComponentsLibForReact }) => {
-        setCtxInited(true)
-        // flag
-        //   ? setCtxInited(true)
-        //   : (flag = true)
-        return TDesignComponentsLibForReact
-      })
-    )
-    // const e1 = Context.global.use(() => import('@zodui/plugin-common')
-    //   .then(({ CommonPluginForReact }) => {
-    //     flag
-    //       ? setCtxInited(true)
-    //       : (flag = true)
-    //     return CommonPluginForReact
-    //   })
-    // )
+    const e0 = Context.global.use(TDesignComponentsLibForReact)
     return () => e0()
   }, [])
+  const listRef = useRef<ListRef>()
 
   return <>
     Hello ZodUI!
-    {ctxInited && <List model={z.object({
-      foo: z.string()
-    })}/>}
+    <br />
+    <button onClick={() => listRef.current.reset()}>
+      Reset
+    </button>
+    <List
+      ref={listRef}
+      model={z.object({
+        foo: z.string().label('hi')
+      })}
+    />
   </>
 }
