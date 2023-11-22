5eb41f3feeb19c7c3f0fbdf1d248a4dad46133e2
yijie4188 <yijie4188@gmail.com>
2023-11-22 23:59:02 +0800

    fix(docs): can't auto scroll to target element

--- demo/src/builder/marked-plugins/header.client.ts
+++ demo/src/builder/marked-plugins/header.client.ts
@@ -1,8 +1,10 @@
 import './header.client.scss'
+
+const hash = decodeURIComponent(location.hash)
 document.querySelectorAll<HTMLDivElement>('.anchor-point')
-  .forEach(() => {
-    const hash = window.location.hash
-    if (hash) {
+  .forEach(e => {
+    const id = e.id
+    if (hash && id === hash.slice(1)) {
       document.querySelector(hash)?.scrollIntoView({
         behavior: 'smooth'
       })
