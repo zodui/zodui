1d0c3d97b717a90df1e19cf40486b717033904a1
yijie4188 <yijie4188@gmail.com>
2023-11-28 03:14:50 +0800

    fix(react): schema options label compute error

--- packages/react/src/switchers/complex.tsx
+++ packages/react/src/switchers/complex.tsx
@@ -18,7 +18,7 @@ import { Switcher } from './index'
 function resolveSchemas(schemas: ZodUnionOptions): ComponentProps.Option[] {
   // TODO resolve not literal type, it not contain value
   return schemas.map((schema, index) => ({
-    label: schema._def.label || schema._def.description || schema._def.value || `${index}nd`,
+    label: schema._def.label ?? schema._def.description ?? JSON.stringify(schema._def.value) ?? `${index}nd`,
     title: schema._def.description,
     value: index
   }))
