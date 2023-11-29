3097e5f4c8890dff9438f8fecb5786967d30570d
yijie4188 <yijie4188@gmail.com>
2023-11-30 01:26:33 +0800

    feat(docs): support copy to clipboard

--- demo/src/builder/marked-plugins/copy.client.ts
+++ demo/src/builder/marked-plugins/copy.client.ts
@@ -1,1 +1,9 @@
 import './copy.client.scss'
+
+document.querySelectorAll('.copy-btn').forEach(btn => {
+  btn.addEventListener('click', () => {
+    const text = btn.getAttribute('data-clipboard-text')
+    if (!text) return
+    navigator.clipboard.writeText(text)
+  })
+})
--- demo/src/builder/marked-plugins/copy.ts
+++ demo/src/builder/marked-plugins/copy.ts
@@ -4,7 +4,7 @@ export default defineMDPlugin({
   code(code, lang) {
     if (lang.includes('no-copy')) return false
     return `<div class='copy-wrap'>
-      <div class='copy-btn material-icons'>
+      <div class='copy-btn material-icons' data-clipboard-text="${escapeHTML(code)}" title='Copy'>
         content_copy
       </div>
       ${marked.parse(`\`\`\`${lang} no-copy\n${code}\n\`\`\``)}
@@ -13,3 +13,12 @@ export default defineMDPlugin({
 }, [
   '/src/builder/marked-plugins/copy.client.ts'
 ])
+
+function escapeHTML(html: string) {
+  return html
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;') // "
+    .replace(/'/g, '&#039;') // '
+    .replace(/`/g, '&#x60;') // `
+}
