67b3971c32306ff8368c3bcacf40473f1b72c52a
yijie4188 <yijie4188@gmail.com>
2023-11-29 23:57:50 +0800

    feat(docs): support switch tab

--- demo/src/builder/marked-plugins/tabs.client.ts
+++ demo/src/builder/marked-plugins/tabs.client.ts
@@ -1,1 +1,13 @@
 import './tabs.client.scss'
+
+document.querySelectorAll<HTMLDivElement>('.docs-tabs').forEach(tabs => {
+  const header = tabs.querySelector('.docs-tabs__header')
+  const headerItems = header.querySelectorAll('.docs-tabs__header-item')
+  header.querySelectorAll('.docs-tabs__header-item').forEach((item, index) => {
+    item.addEventListener('click', () => {
+      headerItems.forEach(item => item.classList.remove('active'))
+      item.classList.add('active')
+      tabs.style.setProperty('--active-index', index.toString())
+    })
+  })
+})
