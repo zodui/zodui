b3d49af64664889a2d57dcbcd84c5492dc026c00
yijie4188 <yijie4188@gmail.com>
2023-12-03 04:14:04 +0800

    chore(test): enable all test cases

--- test-for-published/react+td/src/app.tsx
+++ test-for-published/react+td/src/app.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable comma-dangle */
 import '@zodui/react/index.css'
 import '@zodui/components-lib-tdesign/index.css'
 
@@ -16,7 +17,7 @@ export default function App() {
   const listRef = useRef<ListRef>()
   const [value, setValue] = useState({
     boz: { path: '2' },
-    bar: [{ path: '2' }]
+    bar: { path: '2' }
   })
 
   return <>
@@ -28,30 +29,30 @@ export default function App() {
     <List
       ref={listRef}
       model={z.object({
-        // foo: z.string(),
+        foo: z.string(),
         fuo: z.string().default('1'),
         bar: z.record(z.string()),
         bmr: z.array(z.object({
           path: z.boolean(),
           type: z.string()
         })).mode('wrap'),
-        // baz: z.union([z.string(), z.number()]),
-        // bor: z.discriminatedUnion('type', [
-        //   z.object({
-        //     path: z.string(),
-        //     type: z.literal('1')
-        //   }),
-        //   z.object({
-        //     path: z.string(),
-        //     name: z.string(),
-        //     type: z.literal('2')
-        //   })
-        // ])
+        baz: z.union([z.string(), z.number()]),
+        bor: z.discriminatedUnion('type', [
+          z.object({
+            path: z.string(),
+            type: z.literal('1')
+          }),
+          z.object({
+            path: z.string(),
+            name: z.string(),
+            type: z.literal('2')
+          })
+        ]),
         boz: z.object({
           path: z.string(),
           type: z.string()
         }).mode('wrap'),
-        // bkr: z.record(z.string(), z.string()),
+        bkr: z.record(z.string(), z.string()),
       })}
       value={value}
       onChange={v => {
