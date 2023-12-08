7367d86b5966088b9dcf2389e83b94d62069fa01
yijie4188 <yijie4188@gmail.com>
2023-12-08 18:25:08 +0800

    refactor(rollup-helper): autodetect `exportsEntries`

--- packages-inner/rollup-helper/rollup.template.ts
+++ packages-inner/rollup-helper/rollup.template.ts
@@ -20,6 +20,7 @@ function resolveWorkspacePath(p: string) {
 const pkgJson = JSON.parse(fs.readFileSync(resolve(process.cwd(), 'package.json'), 'utf-8')) as {
   name: string
   exports?: Record<string, {
+    import: string
     'inner-src': string
   }>
 }
@@ -28,7 +29,10 @@ const namePrefix = pkgJson
   .replace(/[@|/-](\w)/g, (_, $1) => $1.toUpperCase())
 const exportsEntriesFromPkgJSON = Object.fromEntries(
   Object.entries(pkgJson.exports)
-    .filter(([key]) => !key.endsWith('.json'))
+    // filter static files
+    .filter(([key]) => !/\.(json|css|scss)$/.test(key))
+    // filter no `inner-src` or `import` field entries
+    .filter(([, value]) => value['inner-src'] && value['import'])
     .map(([key, value]) => [
       key
         .replace(/^\.$/, 'index')
@@ -37,24 +41,27 @@ const exportsEntriesFromPkgJSON = Object.fromEntries(
     ])
 )
 
-export default (exportsEntries: Record<string, string> = exportsEntriesFromPkgJSON, {
-  styled = false,
-  plugins: {
-    index: indexPlugins = [],
-    entry: entryPlugins = [],
-    dts: dtsPlugins = []
-  } = {}
-}: {
-  /**
-   * include styles files
-   */
-  styled?: boolean
-  plugins?: {
-    index?: InputPluginOption
-    entry?: InputPluginOption
-    dts?: InputPluginOption
-  }
-} = {}) => {
+export default (
+  {
+    styled = false,
+    plugins: {
+      index: indexPlugins = [],
+      entry: entryPlugins = [],
+      dts: dtsPlugins = []
+    } = {}
+  }: {
+    /**
+     * include styles files
+     */
+    styled?: boolean
+    plugins?: {
+      index?: InputPluginOption
+      entry?: InputPluginOption
+      dts?: InputPluginOption
+    }
+  } = {},
+  exportsEntries: Record<string, string> = exportsEntriesFromPkgJSON
+) => {
   const [globalsRegister, globalsOutput] = createGlobalsLinkage()
   const external = externalResolver()
 
--- packages/components-lib/tdesign/rollup.config.ts
+++ packages/components-lib/tdesign/rollup.config.ts
@@ -2,15 +2,8 @@ import rollupTemplate from 'rollup-helper/rollup.template'
 import copy from 'rollup-plugin-copy'
 
 export default rollupTemplate({
-  index: 'src/index.ts',
-  react: 'src/react/index.tsx'
-}, {
   styled: true,
   plugins: {
-    dts: copy({
-      targets: [
-        { src: 'src/react.fix.d.ts', dest: 'dist' }
-      ]
-    })
+    dts: copy({ targets: [{ src: 'src/react.fix.d.ts', dest: 'dist' }] })
   }
 })
