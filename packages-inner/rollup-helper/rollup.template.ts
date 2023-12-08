22f7b3c5444ae3181df41fd5c258ff39f9e92fa7
yijie4188 <yijie4188@gmail.com>
2023-12-08 15:39:52 +0800

    refactor: abstract rollup build config

---
+++ packages-inner/rollup-helper/rollup.template.ts
@@ -0,0 +1,112 @@
+import fs from 'node:fs'
+import { resolve } from 'node:path'
+
+import autoprefixer from 'autoprefixer'
+import { getWorkspaceDir } from 'pnpm-helper/getWorkspaceDir'
+import type { InputPluginOption, RollupOptions } from 'rollup'
+import { createGlobalsLinkage } from 'rollup-helper/plugins/globals'
+import skip from 'rollup-helper/plugins/skip'
+import { commonOutputOptions } from 'rollup-helper/utils/commonOptions'
+import externalResolver from 'rollup-helper/utils/externalResolver'
+import { dts } from 'rollup-plugin-dts'
+import esbuild from 'rollup-plugin-esbuild'
+import postcss from 'rollup-plugin-postcss'
+
+const workspaceRoot = getWorkspaceDir()
+function resolveWorkspacePath(p: string) {
+  return resolve(workspaceRoot, p)
+}
+
+const pkgJson = JSON.parse(fs.readFileSync(resolve(process.cwd(), 'package.json'), 'utf-8')) as {
+  name: string
+}
+const namePrefix = pkgJson.name.replace(/^[@/-](\w)/, (_, $1) => $1.toUpperCase())
+
+export default (exportsEntries: Record<string, string>, {
+  styled = false,
+  plugins: {
+    index: indexPlugins = [],
+    entry: entryPlugins = [],
+    dts: dtsPlugins = []
+  } = {}
+}: {
+  /**
+   * include styles files
+   */
+  styled?: boolean
+  plugins?: {
+    index?: InputPluginOption
+    entry?: InputPluginOption
+    dts?: InputPluginOption
+  }
+}) => {
+  const [globalsRegister, globalsOutput] = createGlobalsLinkage()
+  const external = externalResolver()
+
+  return [
+    {
+      input: exportsEntries,
+      output: [
+        {
+          ...commonOutputOptions,
+          format: 'esm',
+          entryFileNames: '[name].esm.js',
+          preserveModules: true
+        }
+      ],
+      plugins: [
+        globalsRegister({ external }),
+        styled && skip({ patterns: [/\.s?css$/] }),
+        esbuild(),
+        indexPlugins
+      ]
+    },
+    ...Object.entries(exportsEntries).map(([name, input]) => ({
+      input: input,
+      output: [
+        {
+          ...commonOutputOptions,
+          name: namePrefix + name === 'index' ? '' : (
+            name[0].toUpperCase() + name.slice(1)
+          ),
+          format: 'iife',
+          entryFileNames: `${name}.iife.js`
+        },
+        {
+          ...commonOutputOptions,
+          name: namePrefix + name === 'index' ? '' : (
+            name[0].toUpperCase() + name.slice(1)
+          ),
+          format: 'umd',
+          entryFileNames: `${name}.umd.js`
+        }
+      ],
+      plugins: [
+        globalsOutput,
+        styled && postcss({
+          plugins: [autoprefixer],
+          minimize: true,
+          sourceMap: true,
+          extract: `${name}.css`
+        }),
+        esbuild(),
+        entryPlugins
+      ],
+      external
+    })),
+    {
+      input: exportsEntries,
+      output: {
+        dir: 'dist',
+        entryFileNames: ({ name }) => `${name.replace(/^src\//, '')}.d.ts`,
+        preserveModules: true
+      },
+      plugins: [
+        styled && skip({ patterns: [/\.s?css$/] }),
+        dts({ tsconfig: resolveWorkspacePath('tsconfig.dts.json') }),
+        dtsPlugins
+      ],
+      external
+    }
+  ] as RollupOptions[]
+}
--- packages/components-lib/tdesign/rollup.config.ts
+++ packages/components-lib/tdesign/rollup.config.ts
@@ -1,94 +1,16 @@
-import autoprefixer from 'autoprefixer'
-import { resolve } from 'path'
-import { getWorkspaceDir } from 'pnpm-helper/getWorkspaceDir'
-import type { RollupOptions } from 'rollup'
-import { createGlobalsLinkage } from 'rollup-helper/plugins/globals'
-import skip from 'rollup-helper/plugins/skip'
-import { commonOutputOptions } from 'rollup-helper/utils/commonOptions'
-import externalResolver from 'rollup-helper/utils/externalResolver'
+import rollupTemplate from 'packages-inner/rollup-helper/rollup.template'
 import copy from 'rollup-plugin-copy'
-import { dts } from 'rollup-plugin-dts'
-import esbuild from 'rollup-plugin-esbuild'
-import postcss from 'rollup-plugin-postcss'
 
-const workspaceRoot = getWorkspaceDir()
-function resolveWorkspacePath(p: string) {
-  return resolve(workspaceRoot, p)
-}
-const [globalsRegister, globalsOutput] = createGlobalsLinkage()
-
-const external = externalResolver()
-
-const exportsEntries = {
+export default rollupTemplate({
   index: 'src/index.ts',
   react: 'src/react/index.tsx'
-}
-
-export default [
-  {
-    input: exportsEntries,
-    output: [
-      {
-        ...commonOutputOptions,
-        format: 'esm',
-        entryFileNames: '[name].esm.js',
-        preserveModules: true
-      }
-    ],
-    plugins: [
-      globalsRegister({ external }),
-      skip({ patterns: [/\.s?css$/] }),
-      esbuild()
-    ]
-  },
-  ...Object.entries(exportsEntries).map(([name, input]) => ({
-    input: input,
-    output: [
-      {
-        ...commonOutputOptions,
-        name: 'ZoduiComponentsLibTDesign' + name === 'index' ? '' : (
-          name[0].toUpperCase() + name.slice(1)
-        ),
-        format: 'iife',
-        entryFileNames: `${name}.iife.js`
-      },
-      {
-        ...commonOutputOptions,
-        name: 'ZoduiComponentsLibTDesign' + name === 'index' ? '' : (
-          name[0].toUpperCase() + name.slice(1)
-        ),
-        format: 'umd',
-        entryFileNames: `${name}.umd.js`
-      }
-    ],
-    plugins: [
-      globalsOutput,
-      postcss({
-        plugins: [autoprefixer],
-        minimize: true,
-        sourceMap: true,
-        extract: `${name}.css`
-      }),
-      esbuild()
-    ],
-    external
-  })),
-  {
-    input: exportsEntries,
-    output: {
-      dir: 'dist',
-      entryFileNames: ({ name }) => `${name.replace(/^src\//, '')}.d.ts`,
-      preserveModules: true
-    },
-    plugins: [
-      skip({ patterns: [/\.s?css$/] }),
-      dts({ tsconfig: resolveWorkspacePath('tsconfig.dts.json') }),
-      copy({
-        targets: [
-          { src: 'src/react.fix.d.ts', dest: 'dist' }
-        ]
-      })
-    ],
-    external
+}, {
+  styled: true,
+  plugins: {
+    dts: copy({
+      targets: [
+        { src: 'src/react.fix.d.ts', dest: 'dist' }
+      ]
+    })
   }
-] as RollupOptions[]
+})
